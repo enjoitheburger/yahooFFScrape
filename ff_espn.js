74a37eb5248eec3bb588a8da73d769493164d1fb
Sebastian Ruiz <sruiz@athenahealth.com>
2018-09-15 14:38:50 -0500

    front end code

--- ff_espn.js
+++ ff_espn.js
@@ -1,38 +1,49 @@
 'use strict'
 const scrapeIt = require("scrape-it")
-const url = 'http://games.espn.com/ffl/schedule?leagueId=165105'
+const url = 'http://games.espn.com/ffl/schedule?leagueId=165105&seasonId=2017'
 
 scrapeIt(url, {
     rows: {
-        listItem: 'tr',
+        listItem: 'tr[bgcolor=#f2f2e8], tr[bgcolor=#f8f8f2]',
         data: {
-            away: 'td:nth-child(1)',
-            home: 'td:nth-child(4)',
+            away: 'td:nth-child(1) > a',
+            home: 'td:nth-child(4) > a',
             result: 'td:nth-child(6)',
         }
     },
 }).then(data => {
 
-    data.rows.splice(0, 1)
+    const output = {}
+    //data.rows.splice(0, 1)
     let managers = {}
     let week = 0
     for (let row of data.rows) {
-        if (row.away && row.home && row.result && row.result !== 'Preview') {
-            if (!managers[row.away]) {
-                managers[row.away] = []
+        const awayManagerName = row.away
+        const homeManagerName = row.home
+        if (awayManagerName && homeManagerName && row.result && row.result !== 'Preview') {
+            // Initialize scores array
+            if (!managers[awayManagerName]) {
+                managers[awayManagerName] = []
             }
-            if (!managers[row.home]) {
-                managers[row.home] = []
+            if (!managers[homeManagerName]) {
+                managers[homeManagerName] = []
             }
 
             const results = row.result.split('-')
-            managers[row.away].push(Number(results[0]))
-            managers[row.home].push(Number(results[1]))
+            managers[awayManagerName].push(Number(results[0]))
+            managers[homeManagerName].push(Number(results[1]))
+
+            if (results[0] > results[1]) {
+                output[awayManagerName] ? output[awayManagerName].actualWins++ : output[awayManagerName] = { actualWins: 0 }
+            } else {
+                output[homeManagerName] ? output[homeManagerName].actualWins++ : output[homeManagerName] = { actualWins: 0 }
+            }
         }
     }
     
     const managerNames = Object.keys(managers)
-    const output = {}
+    const totalWeeklyGames = (managerNames.length - 1)
+    
     managerNames.forEach(name => {
         const gamesSoFar = managers[name].length
         const wins = Array(gamesSoFar).fill(0)
@@ -47,27 +58,19 @@ scrapeIt(url, {
                 }
             })
         })
-        // TODO:
-        const totalWeeklyGames = (managerNames.length - 1)
-        //console.log(name)
-        wins.forEach((numOfWins, index) => {
-            //console.log("Week:", index+1)
-            //console.log(Number(numOfWins/totalWeeklyGames).toFixed(2))
-        })
 
         const totalWins = wins.reduce((prev, curr) => {
             return prev += curr
         }, 0)
 
-        output[name] = {
-            totalWins: totalWins,
-            totalGames: gamesSoFar*totalWeeklyGames,
-        }
+        output[name].totalWins = totalWins
+        output[name].totalGames = gamesSoFar * totalWeeklyGames
+        output[name].totalNumberOfWeeks = gamesSoFar
         // console.log("Total Wins", totalWins)
         // console.log("Total Win PCT:", Number(totalWins/(gamesSoFar*totalWeeklyGames)).toFixed(2))
         // console.log("-----------------------------")
     })
-
+    
     console.log(JSON.stringify(output))
 })
 
